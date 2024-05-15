7761768c3bce800d8e97d82fe478df74222b43d9
AlexandreGerault <AlexandreGerault@users.noreply.github.com>
2024-05-15 05:30:33 +0000

    Fix styling

--- tests/Feature/Workbench/Geocoding/GeocodingDeserializationTest.php
+++ tests/Feature/Workbench/Geocoding/GeocodingDeserializationTest.php
@@ -1,16 +1,9 @@
 <?php
 
-use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Workbench\App\Geocoding\Api\DataTransferObject\Feature;
 use Workbench\App\Geocoding\Api\DataTransferObject\FeatureCollection;
 use Workbench\App\Geocoding\Api\DataTransferObject\ResponseType;
-use Workbench\App\Geocoding\Api\Serialization\CoordinatesNormalizer;
 
 it('deserializer geojson response', function () {
     $json = file_get_contents(__DIR__.'/fixtures/response.json');
