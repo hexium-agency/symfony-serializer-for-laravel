ff6b25ae9442c8a0873f5d2c9869c0f5c1fa61be
Alexandre Gérault <gerault-alexandre@orange.fr>
2024-05-15 07:29:50 +0200

    chore: remove comments

--- tests/Feature/Workbench/Geocoding/GeocodingDeserializationTest.php
+++ tests/Feature/Workbench/Geocoding/GeocodingDeserializationTest.php
@@ -6,6 +6,7 @@ use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 use Workbench\App\Geocoding\Api\DataTransferObject\Feature;
 use Workbench\App\Geocoding\Api\DataTransferObject\FeatureCollection;
 use Workbench\App\Geocoding\Api\DataTransferObject\ResponseType;
@@ -14,17 +15,7 @@ use Workbench\App\Geocoding\Api\Serialization\CoordinatesNormalizer;
 it('deserializer geojson response', function () {
     $json = file_get_contents(__DIR__.'/fixtures/response.json');
 
-    //    $typeExtractor = new \Symfony\Component\PropertyInfo\PropertyInfoExtractor(typeExtractors: [new PhpDocExtractor()]);
-    //
-    //    $serializer = new Serializer(
-    //        normalizers: [
-    //            new BackedEnumNormalizer(),
-    //            new CoordinatesNormalizer(),
-    //            new ArrayDenormalizer(),
-    //            new ObjectNormalizer(propertyTypeExtractor: $typeExtractor),
-    //        ],
-    //        encoders: [new JsonEncoder()],
-    //    );
+    /** @var SerializerInterface $serializer */
     $serializer = app('serializer');
 
     $response = $serializer->deserialize($json, FeatureCollection::class, 'json');
