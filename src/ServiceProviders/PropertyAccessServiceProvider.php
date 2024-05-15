730b63b0981460e125ed7bbfaba8e8ddb59bf257
Alexandre Gérault <gerault-alexandre@orange.fr>
2024-05-15 07:29:40 +0200

    fix: add info extractor to property access

--- src/ServiceProviders/PropertyAccessServiceProvider.php
+++ src/ServiceProviders/PropertyAccessServiceProvider.php
@@ -2,16 +2,29 @@
 
 namespace HexiumAgency\SymfonySerializerForLaravel\ServiceProviders;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\PropertyInfo\PropertyReadInfoExtractorInterface;
+use Symfony\Component\PropertyInfo\PropertyWriteInfoExtractorInterface;
 
 class PropertyAccessServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind('property_accessor', static function () {
-            return new PropertyAccessor();
+        $this->app->bind('property_accessor', static function (Application $application) {
+            /** @var PropertyReadInfoExtractorInterface $readInfoExtractor */
+            $readInfoExtractor = $application->make(PropertyReadInfoExtractorInterface::class);
+
+            /** @var PropertyWriteInfoExtractorInterface $writeInfoExtractor */
+            $writeInfoExtractor = $application->make(PropertyWriteInfoExtractorInterface::class);
+
+            return new PropertyAccessor(
+                readInfoExtractor: $readInfoExtractor,
+                writeInfoExtractor: $writeInfoExtractor,
+            );
         });
+
         $this->app->alias(abstract: 'property_accessor', alias: 'serializer.property_accessor');
     }
 }
